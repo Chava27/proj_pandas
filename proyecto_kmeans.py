7f9f0ddee6dc75a4b306b81503ce24e63ca242b6
Brandocean <A01424338@itesm.mx>
2021-09-16 18:51:48 -0500

    Programa logrado :D

--- proyecto_kmeans.py
+++ proyecto_kmeans.py
@@ -9,6 +9,7 @@ tabla=pd.read_excel("fiestas_data.xlsx")
 tablaMonterrey=tabla.groupby('zona_invitado').get_group('Monterrey')
 tablaMes_mty = tablaMonterrey['mes']
 tablaMes2_mty = []
+
 for i in tablaMes_mty:
     if i == 'Enero':
         tablaMes2_mty.append(1)
@@ -34,17 +35,19 @@ for i in tablaMes_mty:
         tablaMes2_mty.append(11)
     elif i == 'Diciembre':
         tablaMes2_mty.append(12)
-       
-df = pd.DataFrame(tablaMonterrey, columns = ['cantidad_invitados',tablaMes2_mty])
+
+Data={'x':tablaMonterrey['cantidad_invitados'], 'y': tablaMes2_mty}
+
+df = pd.DataFrame(Data, columns = ['x','y'])
 
 print(df)
-plt.scatter (df['cantidad_invitados'], tablaMes2_mty)
+plt.scatter (df['x'], df['y'])
 plt.show()
 
-kmeans = KMeans(n_clusters=3).fit(df)
+kmeans = KMeans(n_clusters=7).fit(df)
 centroids = kmeans.cluster_centers_
 print(centroids)
 
-plt.scatter(df['cantidad_invitados'],tablaMes2_mty, c=kmeans.labels_.astype(float),s=50,alpha=0.5)
+plt.scatter(df['x'], df['y'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
 plt.scatter(centroids[:,0],centroids[:,1],c="red",s=50)
 plt.show()
