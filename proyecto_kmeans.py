58b7110f81c1badd75712d9b54081d4a62649348
Brandocean <A01424338@itesm.mx>
2021-09-16 18:36:51 -0500

    Casi acabado

--- proyecto_kmeans.py
+++ proyecto_kmeans.py
@@ -7,17 +7,44 @@ tabla=pd.read_excel("fiestas_data.xlsx")
 
 #Generar subtablas Monterrey
 tablaMonterrey=tabla.groupby('zona_invitado').get_group('Monterrey')
-
-df = pd.DataFrame(tablaMonterrey, columns = ['cantidad_invitados','mes'])
+tablaMes_mty = tablaMonterrey['mes']
+tablaMes2_mty = []
+for i in tablaMes_mty:
+    if i == 'Enero':
+        tablaMes2_mty.append(1)
+    elif i == 'Febrero':
+        tablaMes2_mty.append(2)
+    elif i == 'Marzo':
+        tablaMes2_mty.append(3)
+    elif i == 'Abril':
+        tablaMes2_mty.append(4)
+    elif i == 'Mayo':
+        tablaMes2_mty.append(5)
+    elif i == 'Junio':
+        tablaMes2_mty.append(6)
+    elif i == 'Julio':
+        tablaMes2_mty.append(7)
+    elif i == 'Agosto':
+        tablaMes2_mty.append(8)
+    elif i == 'Septiembre':
+        tablaMes2_mty.append(9)
+    elif i == 'Octubre':
+        tablaMes2_mty.append(10)
+    elif i == 'Noviembre':
+        tablaMes2_mty.append(11)
+    elif i == 'Diciembre':
+        tablaMes2_mty.append(12)
+       
+df = pd.DataFrame(tablaMonterrey, columns = ['cantidad_invitados',tablaMes2_mty])
 
 print(df)
-plt.scatter (df['cantidad_invitados'],df['mes'])
+plt.scatter (df['cantidad_invitados'], tablaMes2_mty)
 plt.show()
 
-kmeans = KMeans(n_cluster=3).fit(df)
+kmeans = KMeans(n_clusters=3).fit(df)
 centroids = kmeans.cluster_centers_
 print(centroids)
 
-plt.scatter(df['cantidad_invitados'],df['mes'].c=kmeans.labels_.astype(float),s=50,alpha=0.5)
+plt.scatter(df['cantidad_invitados'],tablaMes2_mty, c=kmeans.labels_.astype(float),s=50,alpha=0.5)
 plt.scatter(centroids[:,0],centroids[:,1],c="red",s=50)
 plt.show()
