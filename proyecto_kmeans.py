9a7161422af681d45d0c30149a43870353cb4c52
Brandocean <A01424338@itesm.mx>
2021-09-16 18:15:03 -0500

    Prueba1 del kmeans

--- proyecto_kmeans.py
+++ proyecto_kmeans.py
@@ -7,14 +7,17 @@ tabla=pd.read_excel("fiestas_data.xlsx")
 
 #Generar subtablas Monterrey
 tablaMonterrey=tabla.groupby('zona_invitado').get_group('Monterrey')
-#print(tablaMonterrey)
-listaInvitados_mty = tablaMonterrey["nombre_festejado"]
-listaMes_mty = tablaMonterrey["mes"]
 
-#print(listaInvitados_mty)
-#print(listaMes_mty)
+df = pd.DataFrame(tablaMonterrey, columns = ['cantidad_invitados','mes'])
 
-Data = ['x': listaInvitados,
-        'y': listaMes_mty]
+print(df)
+plt.scatter (df['cantidad_invitados'],df['mes'])
+plt.show()
 
+kmeans = KMeans(n_cluster=3).fit(df)
+centroids = kmeans.cluster_centers_
+print(centroids)
 
+plt.scatter(df['cantidad_invitados'],df['mes'].c=kmeans.labels_.astype(float),s=50,alpha=0.5)
+plt.scatter(centroids[:,0],centroids[:,1],c="red",s=50)
+plt.show()
