c4edf8a65bbdde30354f064fd7ace3963c90a43f
RoyElProgrammer <87441997+RoyElProgrammer@users.noreply.github.com>
2021-09-16 18:12:55 -0500

    modifica: x,y y se cambian 3 clusters a 4

--- proyecto_kmeans.py
+++ proyecto_kmeans.py
@@ -13,15 +13,13 @@ tablaMonterrey=tabla.groupby('zona_invitado').get_group('Monterrey')
 df = pd.DataFrame(tablaMonterrey, columns = ['total_fiesta','cantidad_invitados'])
 
 print (df)
-plt.scatter (df["x"],df["y"])
+plt.scatter (df['cantidad_invitados'],df['total_fiesta'])
 plt.show()
 
-kmeans = KMeans(n_clusters=3).fit(df)
+kmeans = KMeans(n_clusters=4).fit(df)
 centroids = kmeans.cluster_centers_
 print(centroids)
 
-plt.scatter(df["x"], df["y"],c=kmeans.labels_.astype(float),s=50,alpha=0.5)
+plt.scatter(df['cantidad_invitados'], df['total_fiesta'],c=kmeans.labels_.astype(float),s=50,alpha=0.5)
 plt.scatter(centroids[:,0],centroids[:,1],c="red",s=50)
 plt.show()
-
-
