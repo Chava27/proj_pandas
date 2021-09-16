1008e0d1e7eea4acd1b4b8971fa05cf03add7ba6
Brandocean <A01424338@itesm.mx>
2021-09-16 17:46:40 -0500

    Inicio de la branch de Roy

--- proyecto_kmeans.py
+++ proyecto_kmeans.py
@@ -1,6 +1,15 @@
+#16-09-2021
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+#importar tabla de excel
+tabla=pd.read_excel("fiestas_data.xlsx")
+
+#Generar subtablas Monterrey
+tablaMonterrey=tabla.groupby('zona_invitado').get_group('Monterrey')
+#print(tablaMonterrey)
+listaInvitados_mty=
 
 
